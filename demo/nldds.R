52cb228b3ec7daddbe9ad7130e48ead5294c4384
alber Sánchez <albhasan@gmail.com>
2014-04-05 19:50:32 +0200

    Figure 3.4

---
+++ demo/nldds.R
@@ -0,0 +1,17 @@
+#----------------------------------------------------------
+#3.2.2 NONLINEAR DISCRETE DYNAMICAL SYSTEMS
+#Figure 3.4
+#pg 72
+#----------------------------------------------------------
+alpha_0 <- 4
+iterations <- 51
+
+theta_0 <- 10
+theta_1 <- 0.25
+alpha_t.vector <- iterate.f3p(eq3.049.alpha_t, alpha_0, theta_0, theta_1, iterations)
+plot(x = 0:(iterations-1), y = alpha_t.vector, type = "l", col = "blue", xlab = "Time", ylab = "", main = "Figure 3.4 a", lty = 1, lwd = 2.5, xaxs = "i")
+
+theta_0 <- 10
+theta_1 <- -0.25
+alpha_t.vector <- iterate.f3p(eq3.049.alpha_t, alpha_0, theta_0, theta_1, iterations)
+plot(x = 0:(iterations-1), y = alpha_t.vector, type = "l", col = "blue", xlab = "Time", ylab = "", main = "Figure 3.4 b", lty = 1, lwd = 2.5, xaxs = "i")
