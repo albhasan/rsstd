404c9c384ce98f133f3c31e84f11d619157f0251
albhasan <albhasan@gmail.com>
2014-10-05 11:30:10 +0200

    Chapter 3

--- demo/srotp.R
+++ demo/srotp.R
@@ -24,7 +24,7 @@ legend(4, 1, c("Original Series", "Filtered Series"), cex=0.8, col=c("blue","gre
 data(list = c("T7UV"), package = c("rsstd"))
 rawinsondesChuuk <- as.vector(unlist(T7UV[1]))
 ######################################################################
-subset <- (1:length(rawinsondesChuuk)) %% 30 == 0
+subset <- (1:length(rawinsondesChuuk)) %% 7 == 0
 rawinsondesChuuk <- subset(rawinsondesChuuk, subset = subset)
 ######################################################################
 T <- length(rawinsondesChuuk)
@@ -37,29 +37,37 @@ for(t in 1:T){
     w_t.vector[t] <- 1
   }
 }
+
+
+.ar.ts <- function(w.vector, ts.vector){
+  p <- length(w.vector)
+  t.vector <- seq(from = p + 1, to = length(ts.vector), by = 1)
+  unlist(lapply(t.vector, .dummy.ar.ts, p = p, w.vector = w.vector, ts.vector = ts.vector))
+}
+.dummy.ar.ts <- function(t, p, w.vector, ts.vector){
+  sum(ts.vector[(t - p):(t - 1)] * w.vector, na.rm = FALSE)
+}
+
 tapperRawinsondesChuuk <- rawinsondesChuuk * w_t.vector
 P <- 50
 k.vector <- seq(from = 0, to = T/2, by = 1)
-#fcircumflex_of_omega_k <- unlist(mclapply(k.vector, eq3.154.fcircumflex_of_omega_k, P = P, Y = tapperRawinsondesChuuk))
+fcircumflex_of_omega_k <- unlist(mclapply(k.vector, eq3.154.fcircumflex_of_omega_k, P = P, Y = tapperRawinsondesChuuk))
+
+tapperRawinsondesChuuk.ar24 <- ar(tapperRawinsondesChuuk, aic = FALSE, order.max = 24)
+tapperRawinsondesChuuk.ar24.ts <- .ar.ts(w.vector = tapperRawinsondesChuuk.ar24[[2]], ts.vector = tapperRawinsondesChuuk)
+fcircumflex_of_omega_k.ar24 <- unlist(mclapply(k.vector, eq3.154.fcircumflex_of_omega_k, P = P, Y = tapperRawinsondesChuuk.ar24.ts))
 
 
-tapperRawinsondesChuuk.ar24 <- ar(tapperRawinsondesChuuk, order.max = 24)
+plot(x = fcircumflex_of_omega_k, type = "l", col = "red", xlab = "Frequency (cycle/day)", ylab = "Power Spectral Density: (ms-1)2 day", main = "Figure 3.20", lty = 1, lwd = 2.5, xaxs = "i",  pch = 21)
+lines(x = fcircumflex_of_omega_k.ar24, type = "l", col = "green", pch = 22, lty = 2, lwd = 2.5)
+legend(100, 1500, c("Smoothed Periodogram", "AR Spectrum"), cex=0.8, col=c("red","green"), pch=21:22, lty=1:2);
 
-plot(x = fcircumflex_of_omega_k, type = "l", col = "red", xlab = "Time", ylab = "", main = "Figure 3.19", lty = 1, lwd = 2.5, xaxs = "i",  pch = 21)
+# Validation
+#library(TSA)
+#par(mfrow = c(2, 1))
+#plot(x = fcircumflex_of_omega_k, type = "l", col = "red", xlab = "Frequency (cycle/day)", ylab = "Power Spectral Density: (ms-1)2 day", main = "Figure 3.20", lty = 1, lwd = 2.5, xaxs = "i",  pch = 21)
+#periodogram(tapperRawinsondesChuuk,ylab='tapperRawinsondesChuuk.ar24.ts');  abline(h=0)
 
-
-ar.ts <- function(w.vector, ts.vector){
-  p <- length(w.vector)
-  p.vector <- seq(from = p, to = length(ts.vector), by = 1)
-  
-}
-
-
-
-#initial_k <- 0
-#omega_k.vector <- eq3.130.omega_k(T, initial_k)
-#contruit funciones con estas
-#eq3.131.phi_k_of_t
-#eq3.132.phi_0_of_t
-#eq3.133.phi_halfT_of_t
-#yluego llamara 3.151
+#plot(x = fcircumflex_of_omega_k.ar24, type = "l", col = "green", pch = 22, lty = 2, lwd = 2.5)
+#periodogram(tapperRawinsondesChuuk.ar24.ts,ylab='tapperRawinsondesChuuk.ar24.ts');  abline(h=0)
+#par(mfrow = c(1, 1))
