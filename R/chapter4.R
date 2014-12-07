91d810fd536202684b14c9357bfae995cffea2cb
albhasan <albhasan@gmail.com>
2014-12-08 00:06:03 +0100

    Semivariograms' workaround

--- R/chapter4.R
+++ R/chapter4.R
@@ -4,14 +4,38 @@
 #' Equation 4.6 - page 126
 #'
 #' @details
-#' No details.
+#' OUT OF ORDER
 #' 
 #' @param h h
 #' @param theta_1 theta_1
 #' @param theta_2 theta_2
 #' @param variance_0 variance_0
 #' @param variance_1 variance_1
-eq4.6.gamma_Y_of_h_and_theta <- function(h, theta_1, theta_2, variance_0, variance_1){
-  norm_of_h <- norm(as.matrix(h))
-  variance_0 * diag(length(h)) * (as.numeric(norm_of_h != 0)) + variance_1 * (2^(theta_2 - 1) * gamma(theta_2))^-1 * (norm_of_h/theta_1)^theta_2 * besselY(x = (norm_of_h/theta_1), nu = theta_2)
+eq4.6.C_Y_of_h_and_theta <- function(h, theta_1, theta_2, variance_0, variance_1){
+  #TODO: It is not working. I couldn't understand the first term of the equation.
+  #norm_of_h <- sqrt(sum(h^2)) # norm(as.matrix(h))
+  #res <- 0
+  #if (norm_of_h != 0){
+  #  res <- variance_0 * diag(x = h, nrow = length(h), ncol = length(h)) * (as.numeric(norm_of_h == 0)) + variance_1 * (2^(theta_2 - 1) * gamma(theta_2))^-1 * (norm_of_h/theta_1)^theta_2 * besselY(x = (norm_of_h/theta_1), nu = theta_2)
+  #}
+  # return(as.vector(res))
+} 
+
+
+
+#' @title Powered-exponential isotropic variogram
+#'
+#' @description
+#' Equation 4.7 - page 127
+#'
+#' @details
+#' OUT OF ORDER
+#' 
+#' @param h h
+#' @param theta_1 theta_1
+#' @param theta_2 theta_2
+#' @param variance_0 variance_0
+#' @param variance_1 variance_1
+eq4.7.gamma_Y_of_h_and_theta <- function(h, theta_1, theta_2, variance_0, variance_1){
+  #TODO: It is not working. I couldn't understand the first term of the equation.
 } 
--- demo/pg127_vccf.R
+++ demo/pg127_vccf.R
@@ -6,19 +6,86 @@
 #4.1.1 The variogram and the covariance function
 #Figure 4.2
 #pg 127
-#NOTE: In the book the y-axis has the wrong label
 #----------------------------------------------------------
-theta_1 <- theta[1]
-theta_2 <- theta[2]
-variance_0 <- theta[3]
-variance_1 <- theta[4]
-h.vector <- seq(from = 0.1, to = 10, by = 0.1)
+theta_1 <- 1
+theta_2 <- 1.5
+variance_0 <- 0.1
+variance_1 <- 0.9
+h.vector <- seq(from = 0.0, to = 10, by = 0.1)
+
+#TODO: eq 4.6 is not working
+gamma_Y_eq46 <- function(h, theta_1, theta_2, variance_0, variance_1){
+  C_Y_of_h_and_theta <- -1
+  if(h > 0){
+    normh <- norm(as.matrix(h))
+    C_Y_of_h_and_theta <- variance_1 * exp(-normh) * (1 + normh)
+  }else{
+    C_Y_of_h_and_theta <- variance_0 + variance_1
+  }
+  return(C_Y_of_h_and_theta)
+}
+
+gamma_Y <- sapply(h.vector, gamma_Y_eq46, theta_1 = theta_1, theta_2 = theta_2, variance_0 = variance_0, variance_1 = variance_1)
+plot(x = h.vector[-1], y = gamma_Y[-1], type = "l", col = "blue", xlab = "h", ylab = "C_Y(h; theta)", main = "Figure 4.2", lty = 1, lwd = 2.5, xaxs = "i", ylim = c(0, 1), xlim = c(0, 10))
+points(x = h.vector[1], y = gamma_Y[1], col = "blue", pch = 16)
 
 
-C_Y_of_0_and_theta <- variance_0 + variance_1
-C_Y_of_h_and_theta <- sapply(h.vector, gamma_Y_of_h, theta_1 = theta_1, theta_2 = theta_2, variance_0 = variance_0, variance_1 = variance_1)
-gamma_Y_of_h_and_theta <- C_Y_of_0_and_theta - C_Y_of_h_and_theta
 
-plot(x = h.vector, y = gamma_Y_of_h_and_theta, type = "l", col = "blue", xlab = "h", ylab = "gamma_Y(h;theta)", main = "Figure 4.2", lty = 1, lwd = 2.5, xaxs = "i", xlim = c(0, 10), ylim = c(0, 1), pch = 21)
-points(x = 0, y = C_Y_of_0_and_theta, col = "blue", pch = 16)
+#----------------------------------------------------------
+#4.1.1 The variogram and the covariance function
+#Figure 4.3
+#pg 128
+#----------------------------------------------------------
+theta_1 <- 1
+theta_2 <- 1
+variance_0 <- 0.1
+variance_1 <- 0.9
+h.vector <- seq(from = 0.0, to = 10, by = 0.1)
 
+#TODO: eq 4.7 is not working
+gamma_Y_eq47 <- function(h, theta_1, theta_2, variance_0, variance_1){
+  C_Y_of_h_and_theta <- -1
+  if(h != 0){
+    normh <- norm(as.matrix(h))
+    C_Y_of_h_and_theta <- variance_1 * exp(-normh)
+  }else{
+    C_Y_of_h_and_theta <- variance_0 + variance_1
+  }
+  return(C_Y_of_h_and_theta)
+}
+
+gamma_Y <- sapply(h.vector, gamma_Y_eq47, theta_1 = theta_1, theta_2 = theta_2, variance_0 = variance_0, variance_1 = variance_1)
+plot(x = h.vector[-1], y = gamma_Y[-1], type = "l", col = "blue", xlab = "h", ylab = "C_Y(h; theta)", main = "Figure 4.3", lty = 1, lwd = 2.5, xaxs = "i", ylim = c(0, 1), xlim = c(0, 10))
+points(x = h.vector[1], y = gamma_Y[1], col = "blue", pch = 16)
+
+
+
+#----------------------------------------------------------
+#4.1.1 The variogram and the covariance function
+#Figure 4.4
+#pg 129
+#----------------------------------------------------------
+theta_1 <- 1
+theta_2 <- 1.5
+variance_0 <- 0.1
+variance_1 <- 0.9
+h.vector <- seq(from = 0.0, to = 10, by = 0.01)
+
+#TODO: eq 4.8 is not working
+gamma_Y_eq48_andHalf <- function(h, theta_1, theta_2, variance_0, variance_1){
+  C_Y_of_h_and_theta <- -1
+  normh <- norm(as.matrix(h))
+  if(h != 0){
+    C_Y_of_h_and_theta <- variance_0 + variance_1 - variance_1 * exp(-normh)
+  }else{
+    #C_Y_of_h_and_theta <- 0 + variance_1 - variance_1 * exp(-normh)
+    C_Y_of_h_and_theta <- variance_0 * diag(x = h, nrow = length(h), ncol = length(h)) * (as.numeric(normh == 0)) + variance_1 - variance_1 * exp(-normh)
+  }
+  
+  
+  return(C_Y_of_h_and_theta)
+}
+
+gamma_Y <- sapply(h.vector, gamma_Y_eq48_andHalf, theta_1 = theta_1, theta_2 = theta_2, variance_0 = variance_0, variance_1 = variance_1)
+plot(x = h.vector[-1], y = gamma_Y[-1], type = "l", col = "blue", xlab = "h", ylab = "gamma_Y(h; theta)", main = "Figure 4.3", lty = 1, lwd = 2.5, xaxs = "i", ylim = c(0, 1), xlim = c(0, 10))
+points(x = h.vector[1], y = gamma_Y[1], col = "blue", pch = 16)
